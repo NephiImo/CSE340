21ca3acaecfc50c0dbffc7636a121019f73d724d
NephiImo <imonephi@gmail.com>
2025-12-09 17:12:19 +0100

    JWT Authorization test1

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -10,7 +10,7 @@ const utilities = require("../utilities")
 const regValidate = require('../utilities/account-validation')
 
 // Account Management View (default account route)
-router.get("/", utilities.handleErrors(accountController.buildAccountManagement))
+router.get("/", utilities.checkLogin, utilities.handleErrors(accountController.buildAccountManagement))
 
 /* **********************************
 * Deliver login view
